a1289db05b454944774263162c8e29ccb77dea12
Epiphany <99079604+tenacioustommy@users.noreply.github.com>
2024-10-05 18:29:55 +0800

    Update install_ssh_server.sh

--- install_ssh_server.sh
+++ install_ssh_server.sh
@@ -15,15 +15,6 @@ fi
 chmod 700 /kaggle/working/.ssh
 chmod 600 /kaggle/working/.ssh/authorized_keys
 
-# Download ngrok
-FILE=/kaggle/working/SSH/ngrok
-if ! test -f "$FILE"; 
-then
-    wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
-    sudo tar xvzf ngrok-v3-stable-linux-amd64.tgz -C /usr/local/bin
-    rm ngrok-v3-stable-linux-amd64.tgz
-fi
-
 # Install SSH-Server
 sudo apt update
 sudo apt install openssh-server -y
